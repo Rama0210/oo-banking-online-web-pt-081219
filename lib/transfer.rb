3e1468db567da9c15065de9339133b33e4cc16d9
upbeat-torrent-2014 <ramaabrahams@yahoo.com>
2019-09-27 00:20:32 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -21,15 +21,12 @@ class Transfer
      @sender.balance -= @amount
      @receiver.balance += @amount
       @status = "complete"
-   # elsif
-   # !@sender.valid?
-   # @status = "rejected"
+  
+  
    else
      @status = "rejected"
      return "Transaction rejected. Please check your account balance."
-     # !@sender.valid? && @status = "rejected"
-     # return "Transaction rejected. Please check your account balance."
-   end
+     
  end
  def reverse_transfer
    if @status == "complete"
